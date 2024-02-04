b4a6ab3ee65eb430277b5dd4782027e1a7fef25c
MohammadAminZamani.afshar <zamani3509@gmail.com>
2024-02-04 18:19:55 +0330

    refactor error handling of launch url function

--- lib/providers/url_handler.dart
+++ lib/providers/url_handler.dart
@@ -4,8 +4,10 @@ import 'package:url_launcher/url_launcher.dart';
 Future<void> launchUrlOnWebBrowser(String url) async {
   try {
     final parsedUrl = Uri.parse(url);
-    await launchUrl(parsedUrl, mode: LaunchMode.externalApplication);
-  } catch (e) {
+    final result = await launchUrl(parsedUrl, mode: LaunchMode.externalApplication);
+    if (result) return;
     debugPrint('Failed to open url: $url');
+  } catch (e) {
+    debugPrint('Something goes wrong when trying to open $url \nerror: $e');
   }
 }
