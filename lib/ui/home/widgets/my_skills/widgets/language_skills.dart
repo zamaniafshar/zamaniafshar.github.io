6e25b7731604d67a76b93beb1938f824e871bfa1
MohammadAminZamani.afshar <zamani3509@gmail.com>
2024-02-08 13:12:05 +0330

    fix: overflow on mobile in language skills

--- lib/ui/home/widgets/my_skills/widgets/language_skills.dart
+++ lib/ui/home/widgets/my_skills/widgets/language_skills.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:personal_website/common/responsive/responsive.dart';
 import 'package:personal_website/config/constants/assets_paths.dart';
 import 'package:personal_website/ui/home/widgets/my_skills/widgets/skills_section.dart';
 
@@ -54,13 +55,17 @@ class LanguageSkill extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final screen = Screen.of(context);
 
     return PhysicalModel(
       color: theme.colorScheme.surface,
       borderRadius: BorderRadius.circular(100),
       elevation: 2,
       child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        padding: EdgeInsets.symmetric(
+          vertical: screen.adaptive(defaultValue: 10, mobile: 5)!,
+          horizontal: screen.adaptive(defaultValue: 20, mobile: 10)!,
+        ),
         child: Row(
           children: [
             SizedBox.square(
