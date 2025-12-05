7a2f32776c47f39794a7510cb53b7bce8417007a
zamaniafshar <zamani3509@gmail.com>
2025-12-05 09:22:47 +0000

    Deploying to gh-pages from @ zamaniafshar/zamaniafshar.github.io@9a3f307c7aa7f1c20c5253c6122d86eeff231600 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4194170243"
+    serviceWorkerVersion: "4023065674"
   }
 });
